c82259cd1256f4331a67e24a6e59620dd072d82c
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-09-05 16:11:41 +0200

    Initial commit

---
+++ pages/events/[...slug].js
@@ -0,0 +1,48 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+
+import EventList from "@/components/events/event-list";
+
+function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numMonth) ||
+    isNaN(numYear) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ){
+    return <p>Ivalid filter. Please Adjust your values!</p>
+  }
+
+  const filteredEvents  = getFilteredEvents({
+    year: numYear, 
+    month: numMonth
+  })
+
+  if (!filteredEvents || filteredEvents.length === 0){
+    return <p>No events found for the chosen filter!</p>
+  }
+  
+  return (
+      <div>
+        <EventList items={filteredEvents}/>
+      </div>
+    );
+}
+
+export default FilteredEventsPage;
