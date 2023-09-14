0ac63b135cd96f2bae0a3b80bddc3992a43e924f
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-09-14 15:07:31 +0200

    commentsUpdated

--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -1,20 +1,15 @@
 import { useRef, useState } from 'react';
 import classes from './new-comment.module.css';
-
 function NewComment(props) {
   const [isInvalid, setIsInvalid] = useState(false);
-
   const emailInputRef = useRef();
   const nameInputRef = useRef();
   const commentInputRef = useRef();
-
   function sendCommentHandler(event) {
     event.preventDefault();
-
     const enteredEmail = emailInputRef.current.value;
     const enteredName = nameInputRef.current.value;
     const enteredComment = commentInputRef.current.value;
-
     if (
       !enteredEmail ||
       enteredEmail.trim() === '' ||
@@ -27,16 +22,14 @@ function NewComment(props) {
       setIsInvalid(true);
       return;
     }
-
     props.onAddComment({
       email: enteredEmail,
       name: enteredName,
       text: enteredComment,
     });
   }
-
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={sendCommentHandler}>
       <div className={classes.row}>
         <div className={classes.control}>
           <label htmlFor='email'>Your email</label>
@@ -56,5 +49,4 @@ function NewComment(props) {
     </form>
   );
 }
-
 export default NewComment;
--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,24 +1,31 @@
-import classes from './newsletter-registration.module.css';
-
+import { useRef } from "react";
+import classes from "./newsletter-registration.module.css";
 function NewsletterRegistration() {
+  const emailInputRef = useRef();
   function registrationHandler(event) {
     event.preventDefault();
-
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
+    fetch("api//newsletter", {
+      method: "POST",
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
-
   return (
     <section className={classes.newsletter}>
       <h2>Sign up to stay updated!</h2>
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
@@ -26,5 +33,4 @@ function NewsletterRegistration() {
     </section>
   );
 }
-
 export default NewsletterRegistration;
