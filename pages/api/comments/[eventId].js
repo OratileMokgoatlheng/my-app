139f453266ca7adde3b67678f33dd51634edd3f7
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-09-17 09:55:06 +0200

    handlingErrors

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,33 +1,63 @@
-function  handler(req, res) {
-    const eventId = req.query.eventId;
-    if (req.method === 'POST'){
-        //add server-side validation
-        const { email, name, text } = req.body;
-        if(
-            !email.includes('@') ||
-            !name ||
-            name.trim() === '' ||
-            !text ||
-            text.trim() === ''
-        ){
-            res.status(422).json({message: 'Invalid input.'});
-            return;
-        }
-        const newComment = {
-            id: new Date().toISOString(),
-            email,
-            name,
-            text
-        }
-        console.log(newComment);
-        res.status(201).json({ message: 'added comment.', comment: newComment})
+import { connectDatabasee, insertDocument, getAllDocuments } from "@/helpers/db-util";
+
+async function handler(req, res) {
+
+  const eventId = req.query.eventId;
+  let client
+  try{
+     client = await connectDatabasee()
+
+  }catch(error){
+res.status(500).json({message: 'Connecting to the database failed!'})
+return
+  }
+
+
+  if (req.method === "POST") {
+    //add server-side validation
+    const { email, name, text } = req.body;
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      client.close();
+      return;
     }
-    if(req.method === 'GET'){
-        const dummyList = [
-            { id: 'c1', name: 'Kea', text: 'A first comment!' },
-            { id: 'c2', name: 'Ratzi', text: 'A second comment!' }
-        ];
-        res.status(200).json({comments: dummyList})
+    const newComment = {
+      email,
+      name,
+      text,
+      eventId,
+    };
+let result
+    try{
+         result = await insertDocument(client, 'comments', newComment)
+
+         newComment._id = result.insertedId;
+         res.status(201).json({ message: "added comment.", comment: newComment });
+    }catch(error){
+        res.status(500).json({message: 'Loading failed!'})
+        
     }
+    
+
+  }
+  if (req.method === "GET") {
+try{
+
+    const documents = await getAllDocuments(client, 'comments', {_id: -1})
+    res.status(201).json({ comments: documents });
+}catch(error){
+    res.status(500).json({message: 'Getting comments failed!'})
+    
+}
+
+    
+  }
+  client.close();
 }
 export default handler;
